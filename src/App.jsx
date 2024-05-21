0000140b82c220eb39537e383805ffd63961beef
Mariana Santos <marianavsgs@gmail.com>
2024-05-21 11:32:00 +0100

    basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { RouterProvider, createHashRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import HomePage from './Pages/Home.jsx'
 import ProjectDetailPage from './Pages/ProjectDetail.jsx'
 import AppLayout from './AppLayout.jsx'
 
-const route = createHashRouter([
+const route = createBrowserRouter([
   {element: <AppLayout/>,
   children:[
     {index: true, element: <HomePage />},
     {path: ':projectId', element: <ProjectDetailPage />}
   ]}
-])
+],{ basename: '/portfolio-mariana'} )
 
 function App() {
   //const [count, setCount] = useState(0)
